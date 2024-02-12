eccce23b619093d417d3b72f1c514fd015444fbf
sawan1k <sawankushwaha222@gmail.com>
2024-02-12 19:09:04 +0530

    footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,105 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsGithub,
+  BsDribbble,
+} from "react-icons/bs";
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-5 border-emerald-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Sawan Blog
+              </span>
+            </Link>
+          </div>
+          <div className="grid grid-cols-3  gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.100jsprojects.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  100 JS Projects
+                </Footer.Link>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Sawan Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.github.com/sawan1k"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Sawan Blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon
+              href="#"
+              icon={BsFacebook}
+              className="hover:text-red-500"
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsInstagram}
+              className="hover:text-red-500"
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsTwitter}
+              className="hover:text-red-500"
+            />
+            <Footer.Icon
+              href="https://github.com/sawan1k"
+              icon={BsGithub}
+              className="hover:text-red-500"
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsDribbble}
+              className="hover:text-red-500"
+            />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
