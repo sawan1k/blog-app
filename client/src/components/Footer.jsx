e7125afcfb8ac31f4e894e07d2e9d792c22ed7fa
sawan1k <sawankushwaha222@gmail.com>
2024-02-12 19:18:45 +0530

    signin

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -22,7 +22,7 @@ export default function FooterCom() {
               </span>
             </Link>
           </div>
-          <div className="grid grid-cols-3  gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+          <div className="grid grid-cols-3 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
             <div>
               <Footer.Title title="About" />
               <Footer.LinkGroup col>
