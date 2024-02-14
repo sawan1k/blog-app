227306dff789f97f7ab9dc99acbf5418af51b81a
sawan1k <sawankushwaha222@gmail.com>
2024-02-14 20:51:46 +0530

    delete post

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,9 +1,9 @@
 import express from "express";
 const router = express.Router();
 import { verifyToken } from "../utils/verifyUser.js";
-import { createpost,getposts } from "../controllers/post.js";
+import { createpost,getposts , deletepost} from "../controllers/post.js";
 
 router.post("/create", verifyToken, createpost);
-router.get("/getposts", verifyToken, getposts);
-
+router.get("/getposts", getposts);
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
 export default router;
