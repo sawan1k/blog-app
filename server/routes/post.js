6fda7012c68e0187aa041f51f278a632608c1c0a
sawan1k <sawankushwaha222@gmail.com>
2024-02-14 20:59:06 +0530

    update post

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,9 +1,17 @@
 import express from "express";
 const router = express.Router();
 import { verifyToken } from "../utils/verifyUser.js";
-import { createpost,getposts , deletepost} from "../controllers/post.js";
+import {
+  createpost,
+  getposts,
+  deletepost,
+  updatepost,
+} from "../controllers/post.js";
 
 router.post("/create", verifyToken, createpost);
 router.get("/getposts", getposts);
-router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
+
+
 export default router;
