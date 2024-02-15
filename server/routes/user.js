51fd44e27e5038e2e6eae35c96bac5fbc92d67c7
sawan1k <sawankushwaha222@gmail.com>
2024-02-15 10:55:32 +0530

    admin dashboard

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { test, updateUser, deleteUser, signout } from "../controllers/user.js";
+import {
+  test,
+  updateUser,
+  deleteUser,
+  signout,
+  getUsers,
+} from "../controllers/user.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -8,4 +14,5 @@ router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 export default router;
