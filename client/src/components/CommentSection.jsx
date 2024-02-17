d0b7f176a62d7a6acc4f21372c45d7fa30daa8be
sawan1k <sawankushwaha222@gmail.com>
2024-02-17 20:18:46 +0530

    show comment

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -3,13 +3,15 @@
 import { useSelector } from "react-redux";
 import { Alert, Button, Textarea } from "flowbite-react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Comment from "./Comment";
 
 export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
   const [comments, setComments] = useState([]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (comment.length > 200) {
@@ -37,6 +39,20 @@ export default function CommentSection({ postId }) {
       setCommentError(error.message);
     }
   };
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getPostComments/${postId}`);
+        if (res.ok) {
+          const data = await res.json();
+          setComments(data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getComments();
+  }, [postId]);
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -88,6 +104,21 @@ export default function CommentSection({ postId }) {
             </Alert>
           )}
         </form>
+      )}
+      {comments.length === 0 ? (
+        <p className="text-sm my-5">No comments yet!</p>
+      ) : (
+        <>
+          <div className="text-sm my-5 flex items-center gap-1">
+            <p>Comments</p>
+            <div className="border border-gray-400 py-1 px-2 rounded-xl">
+              <p>{comments.length}</p>
+            </div>
+          </div>
+          {comments.map((comment) => (
+            <Comment key={comment._id} comment={comment} />
+          ))}
+        </>
       )}
     </div>
   );
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,7 @@ import {
   deleteUser,
   signout,
   getUsers,
+  getUser
 } from "../controllers/user.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -15,4 +16,5 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
 router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
 export default router;
