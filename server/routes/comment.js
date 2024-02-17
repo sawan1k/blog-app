654bdafea2ebf253a2bdbe3c98b761d8465eadf9
sawan1k <sawankushwaha222@gmail.com>
2024-02-17 20:39:23 +0530

    add like

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createComment,  getPostComments } from "../controllers/comment.js";
+import {
+  createComment,
+  getPostComments,
+  likeComment,
+} from "../controllers/comment.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
-router.get('/getPostComments/:postId', getPostComments);
-
+router.get("/getPostComments/:postId", getPostComments);
+router.put("/likeComment/:commentId", verifyToken, likeComment);
 export default router;
