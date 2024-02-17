20cd4e5b3b93c0b1b417ffea1e279ed3a21f7014
sawan1k <sawankushwaha222@gmail.com>
2024-02-18 00:56:04 +0530

    home page

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -29,7 +29,7 @@ export const createpost = async (req, res, next) => {
 export const getposts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
-    const limit = parseInt(req.query.limit) || 1;
+    const limit = parseInt(req.query.limit) || 9;
     const sortDirection = req.query.order === "asc" ? 1 : -1;
     const posts = await Post.find({
       ...(req.query.userId && { userId: req.query.userId }),
